f9d9dbd65d9e1c479508bced46dd1e3121c9a30d
David Houchin <davidghouchin@gmail.com>
2016-12-01 03:21:30 -0800

    Fix issues with decorator cleaning and config loading, add comments, update readme

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-## Whitespace-Plus
+# Whitespace-Plus
 This extension visibly marks all whitespace characters in the active document.
+You can set the extension to display all whitespace characters, or only trailing ones.
+By modifying the configuration, you can add new patterns to be marked, customize the look of the highlights, set the default display mode, and modify the update rate.
 
-To enable, run the command "Enable Whitespace-Plus". Disable with "Disable Whitespace-Plus".
+## Commands
+**whitespace+ toggle**: Toggle the display of the extension.
+**whitespace+ mode**: Change the display mode from either trailing or all characters.
+**whitespace+ config**: Open the configuration file.
 
+## Contributing
 For any bugs or suggestions, please file an issue on [GitHub](https://www.github.com/davidhouchin/whitespace-plus).
 
-Future plans involve making the decorators easily configurable and nicer looking.
+Future plans involve optimizing and cleaning up the way that decorators are updated and displayed.
+
+## License
+[MIT](https://github.com/davidhouchin/whitespace-plus/blob/master/LICENSE)
--- extension.js
+++ extension.js
@@ -5,29 +5,40 @@ var enabled = false;
 var activeEditor;
 
 var config;
-var configPath = vscode.extensions.getExtension("davidhouchin.whitespace-plus").extensionPath + '\\config.json';
+var configPath = vscode.extensions.getExtension("davidhouchin.whitespace-plus").extensionPath + '/config.json';
 var configUri = vscode.Uri.file(configPath);
+var configWatcher = vscode.workspace.createFileSystemWatcher(configPath);
 
 var modes = ["all", "trailing"];
 
 var elements = [];
 
 function activate(context) {
+    // Subscribe to change events from the config file
+    configWatcher.onDidChange(event => {
+        loadConfig(configPath);
+    });
+
+    // Initial configuration load
     loadConfig(configPath);
 
+    // Main toggle method
     var disposable1 = vscode.commands.registerCommand('extension.toggleWhitespacePlus', function () {
+        // If already enabled, clean the decorations and then disable
         if (enabled) {
             cleanDecorations();
             enabled = false;
             return;
         }
 
+        // Otherwise, enable and start the update cycle
         activeEditor = vscode.window.activeTextEditor;
         if (activeEditor) {
             enabled = true;
             triggerUpdate();
         }
         
+        // Also trigger an update on changing the editor
         vscode.window.onDidChangeActiveTextEditor(editor => {
             activeEditor = editor;
             if (editor) {
@@ -35,12 +46,14 @@ function activate(context) {
             }
         }, null, context.subscriptions);
         
+        // And when modifying the document
         vscode.workspace.onDidChangeTextDocument(event => {
             if (activeEditor && event.document === activeEditor.document) {
                 triggerUpdate();
             }
         }, null, context.subscriptions);
         
+        // Set timeout for updating decorations
         var timeout = null;
         function triggerUpdate() {
             if (timeout) {
@@ -52,10 +65,10 @@ function activate(context) {
 
     context.subscriptions.push(disposable1);
     
+    // Show a quick pick menu to change the display mode
     var disposable2 = vscode.commands.registerCommand('extension.toggleWhitespacePlusMode', function () {
         vscode.window.showQuickPick(modes).then(function(selection){
             cleanDecorations();
-            elements.length = 0;
             
             setMode(selection);
 
@@ -65,23 +78,27 @@ function activate(context) {
 
     context.subscriptions.push(disposable2);
 
+    // Open the config file
     var disposable3 = vscode.commands.registerCommand('extension.configWhitespacePlus', function () {
         vscode.workspace.openTextDocument(configUri).then( function(document) {vscode.window.showTextDocument(document)});
     });
     
     context.subscriptions.push(disposable3);
     
+    // Reads editor text, and updates decorators on page
     function updateDecorations() {
-          if ((!activeEditor) || (!enabled)) {
+        if ((!activeEditor) || (!enabled)) {
             return;
         }
 
+        // Create decorators if they don't already exist
         createDecorations();
 
         var text = activeEditor.document.getText();
         var decChars = [];
         var match;
 
+        // For each element, find all patterns in the text and create decorators
         elements.forEach(function(cur, idx) {
             var regex = RegExp(cur.pattern, 'gm');
             decChars[idx] = {
@@ -96,31 +113,36 @@ function activate(context) {
             }
         });
 
+        // Apply decorators to the editor
         decChars.forEach(function(cur) {
             activeEditor.setDecorations(cur.decorator, cur.chars);
         });
     }
 
+    // Load config from config path then cleans and updates decorations
     function loadConfig(filePath) {
         config = JSON.parse(fs.readFileSync(filePath).toString());
-
+        cleanDecorations();
         setMode(config.mode);
-
-        createDecorations();
+        updateDecorations();
     }
 
+    // First deletes elements, then checks mode for trailing element only, or all other elements
     function setMode(mode) {
+        elements.length = 0;                 
+        
         if (mode == "trailing") {
-                config.elements.forEach(function(cur) {
-                    if (cur.name == "trailing") {elements.push(cur)};
-                });
-            } else {
-                config.elements.forEach(function(cur) {
-                    if (cur.enabled) {elements.push(cur)};
-                });
-            }
+            config.elements.forEach(function(cur) {
+                if (cur.name == "trailing") {elements.push(cur)};
+            });
+        } else {
+            config.elements.forEach(function(cur) {
+                if (cur.enabled) {elements.push(cur)};
+            });
+        }
     }
 
+    // Create decorators for the window
     function createDecorations() {
         elements.forEach(function(cur) {
             if (cur.decorator == null) {
@@ -129,9 +151,10 @@ function activate(context) {
         });
     }
 
+    // Clean decorators from the window
     function cleanDecorations() {
         elements.forEach(function(cur) {
-            if (cur.decoration != null) {
+            if (cur.decorator != null) {
                 cur.decorator.dispose();
                 cur.decorator = null;
             }
